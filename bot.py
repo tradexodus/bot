1fb60878a5b7b6d26fe829fa4100ece68ff171ce
tradexodus <tradexodus@gmail.com>
2025-10-06 13:29:02 +0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -100,4 +100,8 @@ async def main():
     await app.run_polling()
 
 if __name__ == "__main__":
+    import nest_asyncio
+    import asyncio
+
+    nest_asyncio.apply()
     asyncio.get_event_loop().run_until_complete(main())
